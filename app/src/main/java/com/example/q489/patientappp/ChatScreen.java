6fe71b93e62a61c1fe9ca1978ece47b395f78a80
Angel Thomas <32402598+AngleThomax@users.noreply.github.com>
2018-11-28 12:00:02 -0800

    Chat Started
    - chat activity added
    - firebase database changed (true)
    - (error) 'cannot resolve symbol 'r'' w/ view

---
+++ app/src/main/java/com/example/q489/patientappp/ChatScreen.java
@@ -0,0 +1,50 @@
+package com.example.q489.patientappp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ChatScreen extends AppCompatActivity {
+
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_screen);
+
+        final TextView messageLog = findViewById(R.id.messageLog);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Message");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                messageLog.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                messageLog.setText("ERROR: MESSAGE NOT SENT");
+            }
+        })
+    }
+
+    public void sendMessage(View view){
+        EditText editText = findViewById(R.id.editText);
+
+        databaseReference.setValue(editText);
+
+    }
+}
