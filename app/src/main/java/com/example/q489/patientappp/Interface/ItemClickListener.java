f6cf073946a6d73de1044e66cb6736820314a4b1
Angel Thomas <32402598+AngleThomax@users.noreply.github.com>
2018-12-05 11:53:09 -0800

    uhhh
    uuhdhhhshfhu

--- app/src/main/java/com/example/q489/patientappp/Interface/ItemClickListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.q489.patientappp.Interface;
-import android.view.View;
-
-public interface ItemClickListener {
-    void onClick(View view, int position, boolean isLongClick);
-}
